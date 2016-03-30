5731cb2d2e3687aed7523cf9f6576b0da4bf408e
dnstrn <denistrinh@gmail.com>
2016-03-29 20:19:45 -0700

    q6 initial commit

---
+++ q6.rb
@@ -0,0 +1,20 @@
+# a.
+
+def even_numbers(n)
+  i = 0
+  while i < n
+    puts i
+    i += 2
+  end
+end
+
+even_numbers(73)
+
+# b.
+
+def even_numbers(n)
+  for i in 0..n
+    i.each do |x|
+      
+
+end
